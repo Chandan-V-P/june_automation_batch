b1c52be75c8cf594e8e58f3d57354e8c95f9b4af
Chandan-V-P <ichandanvp@gmail.com>
2024-07-08 08:40:13 +0530

    First Commit

--- operators/arithmetic_operators.py
+++ operators/arithmetic_operators.py
@@ -1,11 +1,16 @@
 """This module is created to practice Arithmetic operators in python
 created by Chandan on 05/07/2024"""
 
-a = 11
-b = 2
+#PEMDAS Rule or BODMAS Rule
 
-print(a+b)
-print(a-b)
-print(a*b)
-print("a/b --> Normal Division", a/b) #always give decimals #Result --> 5.5
-print("a//b --> floor division", a//b) #it truncate the decimal part --> 5
+a = (8+2)*3/2 #1st Paranthesis --> 10*3/2 --> Mul & Div have same priority (Which ever comes first)
+#10*3/2 --> 30/2 --> 15.0
+print("(8+2)*3/2 is:",a)
+
+b = (8+2)/3**2+3*2
+#10/3**2+3*2 --> 10/9+3*2 --> 10/9+6 --> 1.11+6 --> 7.11
+print("b = (8+2)/3**2+3*2 is:",b)
+
+c = (20+10)*(15/5)
+# 30 * (15/5) --> 30*3.0 --> 90.0
+print("(20+10)*(15/5) is:",c)
---
+++ operators/comparision_operators.py
@@ -0,0 +1,13 @@
+"""This module is created to practice comparision operators in python
+created by Chandan on 08/07/2024"""
+
+#NOT EQUAL (==)
+source_count = 20 #here '=' is assignment operator
+target_count = 20
+print("source_count != target_count",source_count!=target_count) #False
+
+#GREATER THAN
+source_count = 11
+target_count = 8
+print("source_count > target_count",source_count>target_count) #True
+
